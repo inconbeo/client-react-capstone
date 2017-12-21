60d9a32d2d7189b323e309ff7b59d9c43f59e59c
Onesun15 <sunnysharma15@hotmail.com>
2017-12-21 18:43:21 -0500

    refactor for code reveiw

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
-import './landing-page.css'
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import './landing-page.css';
 
 import LoginForm from './login-form';
 
 export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/trending" />;
-    }
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/trending" />;
+  }
 
-    return (
-        <div className="home">
-            {/* <h2>Welcome to Walmart Trending Products App</h2> */}
-            <LoginForm />
-            <p className='newText'>New to eTrendy?</p>
-            <button className='registerAccountButton'><Link to="/register">Create an account</Link></button>
-        </div>
-    );
+  return (
+    <div className="home">
+      <LoginForm />
+      <p className="newText">New to eTrendy?</p>
+      <button className="registerAccountButton">
+        <Link to="/register">Create an account</Link>
+      </button>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './nav-bar.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './nav-bar.css';
 
 export default class NavBar extends Component {
   render() {
     return (
-        <div className='navbar-div'>
-            <nav className='navbar-main'>
-                <div className='navbar-ul'>
-                    <ul className='navbar-list'>
-                        <li className='nav-item'><a className='nav-link' href='/'>Trending Products</a></li>
-                        <li className='nav-item'><Link className='nav-link' to='/dashboard'>Wishlist</Link></li>
-                        {/* <li className='nav-item'><a className='nav-link' href='/sign-in'>Sign In</a></li> */}
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    )
+      <div className="navbar-div">
+        <nav className="navbar-main">
+          <div className="navbar-ul">
+            <ul className="navbar-list">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Trending Products
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">
+                  Wishlist
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
   }
 }
-
